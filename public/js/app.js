f5b1fca5b56c7625060b0edd6b612dcf7ec66efa
rahul.saini <rahul.saini@gen-xt.com>
2022-02-13 17:44:57 +0530

    remove unnecesary console.log

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,5 @@
 
 
-console.log("Client side javascript file is loded")
-
 // fetch('http://puzzle.mead.io/puzzle').then( (response) => {
 //     response.json().then( (data) => {
 //         console.log(data)
